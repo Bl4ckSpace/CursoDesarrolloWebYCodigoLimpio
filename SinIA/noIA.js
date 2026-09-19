3ec1f3a00a43d932bdf41112adc7755a72a15ca5
FJstu <frodpla916@g.educaand.es>
2026-09-19 21:14:26 +0200

    IAYNoIA

--- SinIA/noIA.js
+++ SinIA/noIA.js
@@ -1,3 +1,38 @@
-document.getElementById("boton").addEventListener('click', () => {
-    alert("Esta página no se hizo con IA.")
-})
+let num1
+let num2
+let resultado
+
+function calcular() {
+    num1 = parseFloat(document.getElementById('num1').value);
+    num2 = parseFloat(document.getElementById('num2').value);
+
+    if (document.querySelector('#opciones').value === "+") {
+        comprobarNaN(num1, num2)
+        resultado = num1 + num2
+    }
+    if (document.querySelector('#opciones').value === "-") {
+        comprobarNaN(num1, num2)
+        resultado = num1 - num2
+    }
+    if (document.querySelector('#opciones').value === "x") {
+        comprobarNaN(num1, num2)
+        resultado = num1 * num2
+    }
+    if (document.querySelector('#opciones').value === "/") {
+        comprobarNaN(num1, num2)
+        if (num2 === 0) {
+            alert("¡ERROR! No puedes dividir entre 0.")
+            resultado = ""
+        } else {
+            resultado = num1 / num2
+        }
+    }
+    document.getElementById("resultado").value = resultado
+}
+
+function comprobarNaN(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("Hay un/os campo/s en blanco.");
+        resultado = ""
+    }
+}
